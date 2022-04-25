5fbf37a61bed3251fbf58754cfa9229a16cffff6
Marvin Blum <mb@skalar-design.de>
2022-04-25 16:36:52 +0200

    Finished listing products in category.

--- product_test.go
+++ product_test.go
@@ -1,6 +1,7 @@
 package sw
 
 import (
+	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 )
@@ -8,13 +9,13 @@ import (
 func TestProductClient_ListCategory(t *testing.T) {
 	client, err := NewClient(os.Getenv("SW_HOST"), os.Getenv("SW_ACCESS_KEY"))
 	assert.NoError(t, err)
-	categories, err := client.Category.List(ListCategoryFilter{
+	products, err := client.Product.ListCategory("", ListProductsCategoryFilter{
 		Page:  1,
 		Limit: 10,
 	})
 	assert.NoError(t, err)
-	assert.NotNil(t, categories)
-	result, err := client.Product.ListCategory(categories.Elements[0].Id, ListProductsCategoryFilter{})
+	assert.NotNil(t, products)
+	result, err := client.Product.ListCategory(products.Elements[0].Id, ListProductsCategoryFilter{})
 	assert.NoError(t, err)
 	assert.NotNil(t, result)
 }
--- product_types.go
+++ product_types.go
@@ -1,5 +1,92 @@
 package sw
 
+import "time"
+
+// Product is a product.
+type Product struct {
+	Id                           string    `json:"id"`
+	VersionId                    string    `json:"versionId"`
+	ParentId                     string    `json:"parentId"`
+	ParentVersionId              string    `json:"parentVersionId"`
+	ManufacturerId               string    `json:"manufacturerId"`
+	ProductManufacturerVersionId string    `json:"productManufacturerVersionId"`
+	UnitId                       string    `json:"unitId"`
+	TaxId                        string    `json:"taxId"`
+	CoverId                      string    `json:"coverId"`
+	ProductMediaVersionId        string    `json:"productMediaVersionId"`
+	DeliveryTimeId               string    `json:"deliveryTimeId"`
+	CanonicalProductId           string    `json:"canonicalProductId"`
+	CmsPageId                    string    `json:"cmsPageId"`
+	CmsPageVersionId             string    `json:"cmsPageVersionId"`
+	ProductNumber                string    `json:"productNumber"`
+	Stock                        int64     `json:"stock"`
+	RestockTime                  int64     `json:"restockTime"`
+	Active                       bool      `json:"active"`
+	AvailableStock               int64     `json:"availableStock"`
+	Available                    bool      `json:"available"`
+	IsCloseout                   bool      `json:"isCloseout"`
+	DisplayGroup                 string    `json:"displayGroup"`
+	MainVariantId                string    `json:"mainVariantId"`
+	ManufacturerNumber           string    `json:"manufacturerNumber"`
+	Ean                          string    `json:"ean"`
+	PurchaseSteps                int64     `json:"purchaseSteps"`
+	MaxPurchase                  int64     `json:"maxPurchase"`
+	MinPurchase                  int64     `json:"minPurchase"`
+	PurchaseUnit                 float64   `json:"purchaseUnit"`
+	ReferenceUnit                float64   `json:"referenceUnit"`
+	ShippingFree                 bool      `json:"shippingFree"`
+	MarkAsTopseller              bool      `json:"markAsTopseller"`
+	Weight                       float64   `json:"weight"`
+	Width                        float64   `json:"width"`
+	Height                       float64   `json:"height"`
+	Length                       float64   `json:"length"`
+	ReleaseDate                  time.Time `json:"releaseDate"`
+	RatingAverage                float64   `json:"ratingAverage"`
+	CategoryTree                 []string  `json:"categoryTree"`
+	PropertyIds                  []string  `json:"propertyIds"`
+	OptionIds                    []string  `json:"optionIds"`
+	CategoryIds                  []string  `json:"categoryIds"`
+	ChildCount                   int64     `json:"childCount"`
+	Sales                        int64     `json:"sales"`
+	MetaDescription              string    `json:"metaDescription"`
+	Name                         string    `json:"name"`
+	Keywords                     string    `json:"keywords"`
+	Description                  string    `json:"description"`
+	MetaTitle                    string    `json:"metaTitle"`
+	PackUnit                     string    `json:"packUnit"`
+	PackUnitPlural               string    `json:"packUnitPlural"`
+	//customFields
+	//calculatedPrice
+	//calculatedPrices
+	CalculatedMaxPurchase int64 `json:"calculatedMaxPurchase"`
+	//calculatedCheapestPrice
+	IsNew     bool      `json:"isNew"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	//translated
+	Parent *Product `json:"parent"`
+	//children
+	//deliveryTime
+	//tax
+	//manufacturer
+	//unit
+	//cover
+	//cmsPage
+	//canonicalProduct
+	//media
+	//crossSellings
+	//configuratorSettings
+	//productReviews
+	//mainCategories
+	//seoUrls
+	//options
+	//properties
+	//categories
+	//streams
+	//categoriesRo
+	//seoCategory
+}
+
 // ListProductsCategoryFilter is used to filter products for a category.
 type ListProductsCategoryFilter struct {
 	NoAggregations   string   `json:"no-aggregations"`
@@ -39,6 +126,6 @@ type ListProductsCategoryResult struct {
 	Limit int `json:"limit"`
 	//currentFilters
 	//availableSortings
-	Sorting  string     `json:"sorting"`
-	Elements []Category `json:"elements"`
+	Sorting  string    `json:"sorting"`
+	Elements []Product `json:"elements"`
 }
