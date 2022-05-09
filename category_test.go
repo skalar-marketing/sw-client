14dd8226f2e9f3184cd924bef27772a368183ab6
Marvin Blum <mb@skalar-design.de>
2022-05-09 10:55:01 +0200

    Improved tests.

--- category_test.go
+++ category_test.go
@@ -2,34 +2,42 @@ package sw
 
 import (
 	"github.com/stretchr/testify/assert"
-	"os"
+	"net/http"
 	"testing"
 )
 
 func TestCategory_List(t *testing.T) {
-	client, err := NewClient(os.Getenv("SW_HOST"), os.Getenv("SW_ACCESS_KEY"))
-	assert.NoError(t, err)
+	data := new(ListCategoryFilter)
+	var req *http.Request
+	ts, client := shopwareMock(t, new(ListCategoryResult), data, func(r *http.Request) {
+		req = r
+	})
+	defer ts.Close()
 	result, err := client.Category.List(ListCategoryFilter{
 		Page:  1,
 		Limit: 10,
 	})
+	checkResponse(t, req, "/store-api/category")
+	assert.Equal(t, 1, data.Page)
+	assert.Equal(t, 10, data.Limit)
 	assert.NoError(t, err)
 	assert.NotNil(t, result)
 }
 
 func TestCategoryClient_Get(t *testing.T) {
-	client, err := NewClient(os.Getenv("SW_HOST"), os.Getenv("SW_ACCESS_KEY"))
-	assert.NoError(t, err)
-	categories, err := client.Category.List(ListCategoryFilter{
+	data := new(GetCategoryFilter)
+	var req *http.Request
+	ts, client := shopwareMock(t, new(Category), data, func(r *http.Request) {
+		req = r
+	})
+	defer ts.Close()
+	result, err := client.Category.Get("id", GetCategoryFilter{
 		Page:  1,
 		Limit: 10,
 	})
-	assert.NoError(t, err)
-	assert.NotNil(t, categories)
-	result, err := client.Category.Get(categories.Elements[0].Id, GetCategoryFilter{
-		Page:  1,
-		Limit: 10,
-	})
+	checkResponse(t, req, "/store-api/category/id")
+	assert.Equal(t, 1, data.Page)
+	assert.Equal(t, 10, data.Limit)
 	assert.NoError(t, err)
 	assert.NotNil(t, result)
 }
